ad7573362f5ae3a17d22a07ddd4a0f3ae056f42c
hassohamid <hassohamid@hotmail.com>
2025-04-03 20:07:27 +0200

    Header

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import Header from "./layout/header.jsx";
+
+function App() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default App;
