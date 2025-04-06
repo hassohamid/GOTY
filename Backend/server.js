b0a5291ac202cafac7718623f927c50e03da6e3f
hassohamid <hassohamid@hotmail.com>
2025-04-06 23:22:40 +0200

    BE init

--- Backend/server.js
+++ Backend/server.js
@@ -1,1 +1,20 @@
-import express from "express";
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
+
+app.get("/", (_, res) => {
+  res.send("SUCCESS!");
+});
+
+module.exports = app;
